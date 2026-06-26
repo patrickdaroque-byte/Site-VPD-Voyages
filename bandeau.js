8e6d032288e4e2a0d179bbf0bde85519e133a356
Patrick <patrick.daroque@gmail.com>
2026-06-26 17:27:19 +0200

    Modification Bandeau orange Tarifs…

---
+++ bandeau.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const bandeau = document.getElementById("bandeau-orange");
+    if (bandeau) {
+        bandeau.innerHTML = "Ce tarif comprend : hébergement, repas, visites. Prix indicatif par personne ajusté selon le nombre de personnes. En option : Transport A/R et Guide-Accompagnateur.";
+    }
+});
