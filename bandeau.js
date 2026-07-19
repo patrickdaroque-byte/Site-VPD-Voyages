f8752fb813f13fb1f0a94e743c6332949e8eda3a
patrickdaroque-byte <patrick.daroque@gmail.com>
2026-07-19 20:07:45 +0200

    Corrections renvoi *, bandeau orange et taifs SA

--- bandeau.js
+++ bandeau.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     const bandeau = document.getElementById("bandeau-orange");
     if (bandeau) {
-        bandeau.innerHTML = "Ce tarif comprend : hébergement, repas, visites. Prix indicatif par personne ajusté selon le nombre de personnes. En option : Transport A/R et Guide-Accompagnateur.";
+        bandeau.innerHTML = "Ce tarif comprend : hébergement, repas, visites et transports sur place. Prix indicatif par personne ajusté selon le nombre de participants. En option : Acheminement depuis la France et Guide-Accompagnateur.";
     }
 });
